8d4d52d0182f91c6ba1aaa1d28ef8947781d21c7
abrahim-ali <abrahim71192@gmail.com>
2025-11-05 14:58:38 -0700

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
-const PORT = 3001;
+const PORT = 5000;
 
 // Middleware
 app.use(cors());
