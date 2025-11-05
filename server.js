7d2fba97ee031e9069eb3e742bd4f05762390c21
abrahim-ali <abrahim71192@gmail.com>
2025-11-05 15:36:37 -0700

    Use environment variable for server port

--- server.js
+++ server.js
@@ -23,8 +23,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
-const PORT = 5000;
-
+const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
